4075d9c68c472df13f86912790a04400ccc4bcc2
jarnalyrkar <joachimsolstrom@gmail.com>
2018-04-26 10:09:30 +0200

    last commit

---
+++ Assets/Movement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	public float maxSpeed = 10f;
+
+	Rigidbody2D rigidbody2d;
+
+	void Start () {
+		rigidbody2d = GetComponent<Rigidbody2D>();
+	}
+
+	void FixedUpdate () {
+		float move = Input.GetAxis("Horizontal");
+		rigidbody2d.velocity = new Vector2(move * maxSpeed, -2);
+	}
+}
